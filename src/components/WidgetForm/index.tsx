2a6a39365de9bb8f090b837919821a3c9d6a9f4f
betolarbac <betocr.dev@gmail.com>
2022-05-05 22:24:54 -0300

    finalização de aplicação web de widget

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,69 @@
+import { CloseButton } from "../CloseButton";
+
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/idea.svg';
+import thoughtImageUrl from '../../assets/thought.svg'
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
+
+export const feedbackTypes = {
+    BUG: {
+        title: 'Problema',
+        image: {
+            source: bugImageUrl,
+            alt: 'imagem de inseto'
+        }
+    },
+    IDEA: {
+        title: 'Ideia',
+        image: {
+            source: ideaImageUrl,
+            alt: 'image de uma lampada'
+        }
+    },
+    OTHER: {
+        title: 'Outros',
+        image: {
+            source: thoughtImageUrl,
+            alt: 'nuvem de pensamento'
+        },
+    },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm(){
+    const [ feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+    const [feedbackSent, setFeedbackSent] = useState(false);
+
+    function handleRestartFeedback(){
+        setFeedbackSent(false);
+        setFeedbackType(null);
+    }
+
+    return (
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+          {feedbackSent ? (
+              <FeedbackSuccessStep onFeedbackRestartRequest={handleRestartFeedback} />
+          ) : (
+              <>
+               {! feedbackType ? (
+               <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+           ) : (
+               <FeedbackContentStep feedbackType={feedbackType} 
+                onFeedbackRestartRequest={handleRestartFeedback}
+                onFeedbackSent={() => setFeedbackSent(true)}
+               />
+           )}
+              
+              </>
+          )}
+            
+            <footer className="text-xs text-neutral-400">
+                Feito com ♥ por Roberto
+            </footer>
+        </div>
+    );
+}
